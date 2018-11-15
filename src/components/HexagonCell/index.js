7d4f2e2ebc4ed4ace074d3b9c3803828b9cd97a6
Pixel-Daze <1226093323@qq.com>
2018-11-15 10:46:52 +0800

    hexagon

--- src/components/HexagonCell/index.js
+++ src/components/HexagonCell/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 class HexagonCell extends Component {
     render(){
         return(
-            <div className="hexagon2" style={{backgroundImage: 'url("http://imagev2.xmcdn.com/group43/M00/35/E1/wKgKjVsOoteQfA2TAAJO9RmB-9o070.jpg")'}}>
+            <div className="hexagon2" style={{backgroundImage: 'url("//imagev2.xmcdn.com/group43/M00/35/E1/wKgKjVsOoteQfA2TAAJO9RmB-9o070.jpg")'}}>
                 <div className="hex1"></div>
                 <div className="hex2"></div>
             </div>
